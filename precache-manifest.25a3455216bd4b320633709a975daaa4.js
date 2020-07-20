a2fa0e05681c851752d39a03cd115c5cf3a7b2a0
Michaeldremy <michaeldremy@gmail.com>
2020-07-19 21:26:26 -0700

    Updates

---
+++ precache-manifest.25a3455216bd4b320633709a975daaa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5670872d834dda3dad4a72648d69584",
+    "url": "/COVID-19-Tracker-Application/index.html"
+  },
+  {
+    "revision": "e99c1954e27b70a592e0",
+    "url": "/COVID-19-Tracker-Application/static/css/main.a11b9e02.chunk.css"
+  },
+  {
+    "revision": "f740b1af5fb6294d44f3",
+    "url": "/COVID-19-Tracker-Application/static/js/2.55dde3c8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker-Application/static/js/2.55dde3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99c1954e27b70a592e0",
+    "url": "/COVID-19-Tracker-Application/static/js/main.ec460669.chunk.js"
+  },
+  {
+    "revision": "448236bc003921eb548e",
+    "url": "/COVID-19-Tracker-Application/static/js/runtime-main.25fa97f4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker-Application/static/media/image.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c626d89cb7d824c7f9fcd49a73aac5e.js"
+  "/COVID-19-Tracker-Application/precache-manifest.25a3455216bd4b320633709a975daaa4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-19-Tracker-Application/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
